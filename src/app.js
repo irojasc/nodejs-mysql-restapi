5db9b3cc54d78d21ac4ec1edaa414ac28c36945b
IROJASC <irojasc3@gmail.com>
2023-04-16 23:19:17 -0500

    Se agrega el cors al servidor

--- src/app.js
+++ src/app.js
@@ -1,8 +1,12 @@
 import express from "express";
 import booksRoutes from "./routes/books.routes.js";
+import cors from "cors";
+
 
 
 const app = express();
+
+app.use(cors());
 app.use(express.json());
 app.use('/api',booksRoutes);
 app.use((req, res, next) => {
