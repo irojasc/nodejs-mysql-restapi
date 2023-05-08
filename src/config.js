30e130669d41b8fecac1bf7076b6467e0b6faa42
IROJASC <irojasc3@gmail.com>
2023-05-08 13:04:36 -0500

    actualizacion de credenciales

--- src/config.js
+++ src/config.js
@@ -2,10 +2,10 @@ import { config } from "dotenv";
 
 config();
 
-export const PORT = process.env.PORT || 3000;
+export const PORT = process.env.PORT;
 
-export const DB_HOST = process.env.DB_HOST || 'mysql-28407-0.cloudclusters.net';
-export const DB_USER = process.env.DB_USER || 'admin01';
-export const DB_PASSWORD = process.env.DB_PASSWORD || 'alayza2213';
-export const DB_PORT = process.env.DB_PORT || 28416;
-export const DB_DATABASE = process.env.DB_DATABASE || 'genesisDB';
+export const DB_HOST = process.env.DB_HOST;
+export const DB_USER = process.env.DB_USER;
+export const DB_PASSWORD = process.env.DB_PASSWORD;
+export const DB_PORT = process.env.DB_PORT;
+export const DB_DATABASE = process.env.DB_DATABASE;
