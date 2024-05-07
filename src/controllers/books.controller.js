55e63d1e8e5b9c3def06afa1d0138795dac7ab6f
IROJASC <irojasc3@gmail.com>
2024-05-06 20:05:09 -0500

    ordena products get method by descending order

--- src/controllers/books.controller.js
+++ src/controllers/books.controller.js
@@ -9,7 +9,7 @@ export const getProducts = async (req, res) =>
 export const getProductsbyPattern = async (req, res) => 
 {
     try{
-        const [result] = await pool.query(`select concat(it.code,"_",p.id) as code, p.title as title, p.autor as autor, p.publisher as editorial, wp.pvNew as pv, if((select sum(wp.qtyNew) from genesisDB.ware_product as wp inner join genesisDB.product pe on wp.idProduct = pe.id where wp.idProduct = p.id and wp.isEnabled = True)>0, true, false) as isAvailable from genesisDB.ware_product wp inner join genesisDB.product p on wp.idProduct = p.id inner join genesisDB.item it on p.idItem = it.id where idWare = 4 and isEnabled = True and concat(p.title, " ", p.autor) like '%${req.params.pattern}%';`);
+        const [result] = await pool.query(`select concat(it.code,"_",p.id) as code, p.title as title, p.autor as autor, p.publisher as editorial, wp.pvNew as pv, if((select sum(wp.qtyNew) from genesisDB.ware_product as wp inner join genesisDB.product pe on wp.idProduct = pe.id where wp.idProduct = p.id and wp.isEnabled = True)>0, true, false) as isAvailable from genesisDB.ware_product wp inner join genesisDB.product p on wp.idProduct = p.id inner join genesisDB.item it on p.idItem = it.id where idWare = 4 and isEnabled = True and concat(p.title, " ", p.autor) like '%${req.params.pattern}%' order by isAvailable desc;`);
         
         // if(result.length <= 0) return res.status(404).json({
         //     message: "no book matched"
