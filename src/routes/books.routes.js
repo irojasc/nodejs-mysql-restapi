5eac22e4dbe9151d71e4cfa39fc1715128fa5f2a
IROJASC <irojasc3@gmail.com>
2024-05-15 22:11:50 -0500

    getLastProductByCtgy avance total

--- src/routes/books.routes.js
+++ src/routes/books.routes.js
@@ -1,11 +1,12 @@
 import { Router } from "express";
 
-import { getProducts, createProduct, updateProduct, deleteProduct, getProductsbyPattern } from "../controllers/books.controller.js";
+import { createProduct, updateProduct, deleteProduct, getProductsbyPattern, getLastProductsByCtgy } from "../controllers/books.controller.js";
 
 const router = Router();
 
-router.get('/products', getProducts );
+// router.get('/products', getProducts );
 router.get('/products/:pattern', getProductsbyPattern );
+router.get('/products', getLastProductsByCtgy );
 router.post('/products', createProduct);
 router.put('/products', updateProduct );
 router.delete('/products', deleteProduct);
